5d3c0521e477092e0e1451f118850b8237b504ca
kanatamon <ponde.moonlord@gmail.com>
2021-07-27 13:19:02 +0700

    Correction: tag overflow

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -50,20 +50,23 @@ const Image = styled.img`
 `
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: nowrap;
-  gap: 8px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding: 4px 0;
 `
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
+
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
 `
 
 export default PhotoGridItem
